ff6a55d4e8f01cce8e003fb9ba5fedb2a1efddca
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-02-06 21:04:54 +0800

    add package vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import voie from 'vite-plugin-voie'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    voie()
+    voie(),
+    VitePWA()
   ]
 })
