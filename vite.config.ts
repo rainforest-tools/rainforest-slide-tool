05215338a4d50629b1166b74947e8076c0d93c93
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-02-08 20:40:55 +0800

    add package svg loader to handle heroicons for tailwindcss and support import json files into ts files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import voie from 'vite-plugin-voie'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    voie()
+    voie(),
+    svgLoader()
   ]
 })
